094a97b2a31f54635eb995d19b681a8b3cd347d8
handriss <andrashinkel@gmail.com>
2016-10-21 17:54:30 +0200

    Display class created

---
+++ src/tilegame/Launcher.java
@@ -0,0 +1,10 @@
+package tilegame;
+
+/**
+ * Created by handris on 21.10.16..
+ */
+public class Launcher {
+    public static void main(String[] args){
+        System.out.println("Starting...");
+    }
+}
