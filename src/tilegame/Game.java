386bd6133539dbd6089e1722765a3ac309b92819
handriss <andrashinkel@gmail.com>
2016-10-21 18:28:17 +0200

    game thread added

--- src/tilegame/Game.java
+++ src/tilegame/Game.java
@@ -2,15 +2,66 @@ package tilegame;
 
 import display.Display;
 
-public class Game {
+public class Game implements Runnable{
     private Display display;
 
+    private boolean running = false;
     public int width, height;
+    public String title;
+
+    private Thread thread;
 
     public Game(String title, int width, int height){
         this.width = width;
         this.height = height;
+        this.title = title;
 
+    }
+
+    private void init(){
         display = new Display(title, width, height);
+    }
+
+    private void tick(){
+
+    }
+
+    private void render(){
+
+    }
+
+    public void run(){
+        init();
+
+        while(running){
+            tick();
+            render();
+        }
+
+        stop();
+    }
+
+    public synchronized void start(){
+        if(running){
+            return;
+        }
+        running = true;
+
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    public synchronized void stop(){
+        if(!running){
+            return
+        }
+
+        running = false;
+
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
