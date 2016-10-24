b6d9a416a3b32d148cc8b09a9a8b3272f5c96b88
handriss <andrashinkel@gmail.com>
2016-10-25 00:19:28 +0200

    World generation complete

--- src/states/GameState.java
+++ src/states/GameState.java
@@ -5,27 +5,30 @@ import entities.creatures.Player;
 import gfx.Assets;
 import tilegame.Game;
 import tiles.Tile;
+import worlds.World;
 
 import java.awt.*;
 
 public class GameState extends State {
 
     private Player player;
+    private World world;
 
     public GameState(Game game){
         super(game);
         player = new Player(game, 100, 100);
+        world = new World("res/worlds/world1.txt");
     }
 
     @Override
     public void tick() {
+        world.tick();
         player.tick();
     }
 
     @Override
     public void render(Graphics g) {
-        g.drawImage(Assets.player, 10, 10, null);
+        world.render(g);
         player.render(g);
-        Tile.tiles[0].render(g, 200, 200);
     }
 }
